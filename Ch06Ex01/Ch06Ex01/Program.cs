e4471c43eae83c094ec7d1d3cd9ce3e53a175127
martinnebesar <nebesar.martin@gmail.com>
2020-02-26 17:16:58 -0500

    Ch6 updated

---
+++ Ch06Ex01/Ch06Ex01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Ch06Ex01
+{
+    class Program
+    {
+        static void Write()
+        {
+            Console.WriteLine("Text output from function.");
+        }
+        static void Main(string[] args)
+        {
+            Write();
+            Console.ReadKey();
+        }
+    }
+}
